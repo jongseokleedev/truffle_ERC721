2c6185fe76321fb9b9e9c6c1a906a8beebe5a606
js_lee <jongsuklee45@gmail.com>
2022-06-09 22:24:42 +0900

    first commit

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,8 @@
+const Migrations = artifacts.require("Migrations");
+const MyNFTs = artifacts.require('MyNFTs')
+
+module.exports = function (deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(MyNFTs);
+
+};
